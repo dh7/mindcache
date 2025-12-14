304bd142449cc8352cf0c2590761f0d71bee97f9
dh <dh7777777@gmail.com>
2025-12-14 23:34:36 +0100

    feat: Add KeyPropertiesPanel for managing key attributes including tags, z-index, and value types, and introduce an EditableKeyName component.

--- packages/web/src/app/projects/[projectId]/instances/[instanceId]/components/index.ts
+++ packages/web/src/app/projects/[projectId]/instances/[instanceId]/components/index.ts
@@ -1,3 +1,5 @@
 export { InstanceHeader } from './InstanceHeader';
 export { ActionButtons } from './ActionButtons';
 export { TagFilter } from './TagFilter';
+export { KeyPropertiesPanel } from './KeyPropertiesPanel';
+export { EditableKeyName } from './EditableKeyName';
